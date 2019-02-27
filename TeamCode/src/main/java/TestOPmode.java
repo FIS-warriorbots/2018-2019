c8e5e386a84381d9b9186dd1cf3d910468230c19
Karsten van der Deijl <karsten@vanderdeijl.net>
2019-02-27 12:06:16 +0100

    Update TestOPmode.java

--- TeamCode/src/main/java/TestOPmode.java
+++ TeamCode/src/main/java/TestOPmode.java
@@ -20,7 +20,6 @@ public class TestOPmode extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()){
-            telemetry.addData("cyka blyat", "cyka blyat");  //motto
 
             //getting input from controller for setting motor power
             expectedLeft = -this.gamepad1.left_stick_y;
