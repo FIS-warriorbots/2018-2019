5bdb357717faf1ae909682901dffafecb2d9adff
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2019-03-19 18:25:20 +0100

    added unlock servo capability

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unhook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unhook.java
@@ -5,17 +5,21 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous
 public class unhook extends LinearOpMode {
 
     private DcMotor hook;
 
+    private Servo lock;
+
     private double power = 0.35f;
 
     @Override
     public void runOpMode() {
         hook = hardwareMap.get(DcMotor.class, "HookMotor");
+        lock = hardwareMap.get(Servo.class, "HookLockServo");
 
         hook.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         hook.setPower(power);
@@ -23,6 +27,8 @@ public class unhook extends LinearOpMode {
         waitForStart();
 
         if(opModeIsActive()){
+            lock.setPosition(0.5);
+            sleep(2000);
             hook.setTargetPosition(630);
         }
     }
